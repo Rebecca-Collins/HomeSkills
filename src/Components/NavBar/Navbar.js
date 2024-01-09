c29ba739fbfa5336a141178ceecb6df7d1032854
Rebecca-Collins <rebeccacollins@shaw.ca>
2024-01-08 18:38:38 -0800

    navbar styling

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,38 +1,45 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { BsHouseGearFill } from "react-icons/bs";
+// import navbarImage from "../../assets/images/hero-img.jpg"
 import "./NavBar.scss";
-
 
 function Navbar() {
   return (
-    <section className="navbar">
-<BsHouseGearFill />
-      <div className="navbar__link-container">
-       <Link to='/'>
-       <p>Browse Catagories</p>
-       </Link>
-       <Link to='/'>
-       <p>Blog</p>
-       </Link>
-       <Link to='/'>
-       <p>Write a Review</p>
-       </Link>
-       <Link to='/'>
-       <p>Costs and Financing</p>
-       </Link>
-       <div className="navbar__link-login">
-       <Link to='/'>
-       <p>Log In</p>
-       </Link>
-       <Link to='/'>
-       <p>Sign Up</p>
-       </Link>
-       <Link to='/'>
-       <button>ARE YOU A HOME PRO</button>
-       </Link>
-       </div>
+    <header className="navbar">
+      <div className="navbar__container">
+        <div className="navbar__link-container">
+          <BsHouseGearFill />
+          <Link className="navbar__link" to="/">
+            <p>Browse Catagories</p>
+          </Link>
+          <Link className="navbar__link" to="/">
+            <p>Blog</p>
+          </Link>
+          <Link className="navbar__link" to="/">
+            <p>Write a Review</p>
+          </Link>
+          <Link className="navbar__link" to="/">
+            <p>Costs and Financing</p>
+          </Link>
+        </div>
+        <div className="navbar__link-login">
+          <Link className="navbar__link" to="/">
+            <p>Log In</p>
+          </Link>
+          <Link className="navbar__link" to="/">
+            <p>Sign Up</p>
+          </Link>
+          <Link className="navbar__link" to="/">
+            <button className="navbar__button">ARE YOU A HOME PRO</button>
+          </Link>
+        </div>
       </div>
-    </section>
+      <div>
+      <h1 className="navbar__title">Get matched with local pros for your next project.</h1>
+        <h3>Quickly get quotes from verified and reviewed pros</h3>
+      </div>
+     
+    </header>
   );
 }
 
